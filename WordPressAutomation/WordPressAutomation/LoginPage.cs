930c82c8bea227fd105dc62871fc1485177ecb73
luismunozato <lmemunoz@gmail.com>
2016-11-18 01:20:04 +1100

    Add files via upload

---
+++ WordPressAutomation/WordPressAutomation/LoginPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPressAutomation
+{
+   public  class LoginPage
+    {
+       public void Go()
+       {
+
+           var driver = new FirefoxDriver();
+           driver.Navigate().GoToUrl("https://wordpress.com/wp-login.php");
+
+       }
+    }
+}
---
+++ WordPressAutomation/WordPressTest/LoginTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordPressAutomation;
+
+namespace WordPressTest
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestMethod]
+        public void Admin_User_Can_Login()
+        {
+            var c = new LoginPage();
+            c.Go();
+          //  LoginPage.GoTo();
+          //  LoginPage.LoginAs("admin").WithPassword("password").Login();
+          //
+          //  Assert.IsTrue(DashboardPage.IsAt, "Failed to login.");
+
+        }
+    }
+}
